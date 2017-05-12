a21e6ba09da405cc02b1038082c54e43a936d188
ruimax <811897569@qq.com>
2017-05-12 17:19:11 +0800

    Update data_bing.js

--- data_bing.js
+++ data_bing.js
@@ -1,46 +1,46 @@
-function Observer(data){
-	this.data = data;
-	this.walk(this.data);
-	//this.event = new Event();
+function Observer(data) {
+    this.data = data;
+    this.walk(this.data);
+    //this.event = new Event();
 }
 
-Observer.prototype.walk = function(obj){
-	var val;
-	for(var key in obj){
-		if(obj.hasOwnProperty(key)){
-			val = obj[key];
-			if(isObject(val)){
-				new Observer(val);
-			}
-			this.convert(key,val);
-		}
-	}		
+Observer.prototype.walk = function(obj) {
+    var val;
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            val = obj[key];
+            if (isObject(val)) {
+                new Observer(val);
+            }
+            this.convert(key, val);
+        }
+    }
 }
 
-Observer.prototype.convert = function(key,val){
-	var self = this;
-	Object.defineProperty(this.data,key,{
-		get : function(){
-			console.log("你访问了"+key);
-			return val;
-		},
-		set : function(newVal){
-			console.log("你设置了"+key+",新的值为"+newVal);
-			Event.emit(key,newVal);
-			val = newVal;
-			if(isObject(newVal)){
-				new Observer(val);
-			}
-		}
-	})
+Observer.prototype.convert = function(key, val) {
+    var self = this;
+    Object.defineProperty(this.data, key, {
+        get: function() {
+            console.log("你访问了" + key);
+            return val;
+        },
+        set: function(newVal) {
+            console.log("你设置了" + key + ",新的值为" + newVal);
+            Event.emit(key, newVal);
+            val = newVal;
+            if (isObject(newVal)) {
+                new Observer(val);
+            }
+        }
+    })
 }
 
-Observer.prototype.$watch = function(key,fn){
-	Event.on(key,fn);
+Observer.prototype.$watch = function(key, fn) {
+    Event.on(key, fn);
 }
 
-function isObject(o){
-	return Object.prototype.toString.call(o) == '[object Object]';
+function isObject(o) {
+    return Object.prototype.toString.call(o) == '[object Object]';
 }
 
 // function Event(){
@@ -58,30 +58,30 @@ function isObject(o){
 // 	})
 // }
 var Event = {
-	callbacks:{},
-	on:function(event,fn){
-		this.callbacks[event] = this.callbacks[event] || [];
-	 	this.callbacks[event].push(fn);
-	},
-	emit:function(event){
-		this.callbacks[event] = this.callbacks[event] || [];
-	 	var arg = Array.prototype.slice.call(arguments,1);
-	 	this.callbacks[event].forEach(function(val){
-	 			val.apply(val,arg);
-	 	})	
-	}
+    callbacks: {},
+    on: function(event, fn) {
+        this.callbacks[event] = this.callbacks[event] || [];
+        this.callbacks[event].push(fn);
+    },
+    emit: function(event) {
+        this.callbacks[event] = this.callbacks[event] || [];
+        var arg = Array.prototype.slice.call(arguments, 1);
+        this.callbacks[event].forEach(function(val) {
+            val.apply(val, arg);
+        })
+    }
 }
 
 var app = new Observer({
-  name : {
-	fName: 'y',
-	lName: 'r'
-  },
-  age: 25
+    name: {
+        fName: 'y',
+        lName: 'r'
+    },
+    age: 25
 });
 
-app.$watch('fName',function(name){console.log('name变成了'+name)});
-app.$watch('age',function(age){console.log('age变成了'+age)});
+app.$watch('fName', function(name) { console.log('name变成了' + name) });
+app.$watch('age', function(age) { console.log('age变成了' + age) });
 
 app.data.name.fName = 'yr';
 app.data.age = 100;
